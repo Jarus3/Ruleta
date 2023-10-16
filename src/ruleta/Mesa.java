ec23bf7e8f1688b786200fc6c21c27e222cd97a7
JaruslavMark <87151030+Jarus3@users.noreply.github.com>
2023-10-16 12:50:26 -0400

    mesa

--- src/ruleta/Mesa.java
+++ src/ruleta/Mesa.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ruleta;
-
+import java.util.ArrayList;
 /**
  *
  * @author Jaruslav Markowski
@@ -11,11 +11,25 @@ package ruleta;
 public class Mesa {
     private Numero[] numeros;
     private Ruleta ruleta;
+    private Crupier crupier;
+    private ArrayList<Apuesta> apuestas;
     public Mesa(){
         ruleta=new Ruleta();
         numeros=new Numero[38];
+        apuestas=new ArrayList<Apuesta>();
         for(int i=0;i<38;i++){
             numeros[i]=new Numero(i);
         }
     }
+    public void registrarApuesta(Apuesta apuesta){
+        apuestas.add(apuesta);
+    }
+    public void reiniciarApuestas(){
+        apuestas=new ArrayList<Apuesta>();
+    }
+
+    public ArrayList<Apuesta> getApuestas() {
+        return (ArrayList<Apuesta>)apuestas.clone();
+    }
+    
 }

f7484b17e2d428a4b881a406a3855a7c728f5439
JaruslavMark <87151030+Jarus3@users.noreply.github.com>
2023-10-18 18:14:41 -0400

    apuesta

--- src/ruleta/Apuesta.java
+++ src/ruleta/Apuesta.java
@@ -1,5 +1,45 @@
 package ruleta;
 
+import java.util.ArrayList;
+
 public class Apuesta {
+    protected String nombreUsuario;
+    protected ArrayList<Ficha> fichas;
+    protected float multiplicador;
+    public Apuesta(){
+        fichas=new ArrayList();
+        multiplicador=1;
+        nombreUsuario="";
+    }
+    public Apuesta(String nombreUsuario){
+        fichas=new ArrayList();
+        multiplicador=1;
+        this.nombreUsuario=nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setFichas(ArrayList<Ficha> fichas) {
+        this.fichas = fichas;
+    }
+
+    public void setMultiplicador(float multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public ArrayList<Ficha> getFichas() {
+        return fichas;
+    }
+
+    public float getMultiplicador() {
+        return multiplicador;
+    }
+    
     
 }
